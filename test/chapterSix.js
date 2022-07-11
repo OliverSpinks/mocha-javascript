dedf8098a4869abd7ad5360fd4cd929b6f8ae739
OliverSpinks <oliver-spinks@hotmail.com>
2022-07-11 14:44:03 +0100

    Finished Mocha chapter 6 demo

---
+++ test/chapterSix.js
@@ -0,0 +1,103 @@
+
+var assert = require('assert');
+
+describe('Mathematical Operations - Test Suite', function(){
+
+  this.timeout(10000);
+
+    var a = 10;
+    var b = 10;
+
+  it('Addition of two numbers', function(done){
+
+    var c = a+b;
+
+    assert.equal(c,20);
+
+    setTimeout(done,1000);
+
+
+  });
+
+  it('Subtraction of two numbers', function(done){
+
+    var c = a-b;
+
+    assert.equal(c,0);
+
+    setTimeout(done,2000);
+
+    
+   });
+
+  it('Multiplication of two numbers', function(done){
+
+    var c = a*b;
+
+    assert.equal(c,100);
+
+    setTimeout(done,1000);
+
+
+  });
+
+  it('Division of two numbers', function(done){
+      
+    var c = a/b;
+
+    assert.equal(c,1);
+
+    setTimeout(done,2000);
+
+
+  });
+
+  it('This is a test for Pending Test Feature');
+
+  it('Addition of two numbers', function(done){
+
+    var c = a+b;
+
+    assert.equal(c,20);
+
+    setTimeout(done,1000);
+
+
+  });
+
+  it('Subtraction of two numbers', function(done){
+
+    var c = a-b;
+
+    assert.equal(c,10);
+
+    setTimeout(done,3000);
+
+    
+   });
+
+  it('Multiplication of two numbers', function(done){
+
+    var c = a*b;
+
+    assert.equal(c,100);
+
+    setTimeout(done,2000);
+
+
+  });
+
+  it('Division of two numbers', function(done){
+      
+    var c = a/b;
+
+    assert.equal(c,1);
+
+    setTimeout(done,1000);
+
+
+  });
+
+  it('This is a test for Pending Test Feature');
+
+});
